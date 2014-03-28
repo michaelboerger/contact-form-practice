ca3f3a53a009680b119fd45b0a473c3868c1d252
Michael <michael.boerger@gmail.com>
2014-03-28 15:06:44 -0400

    made all the validations and tests for contact inquiry

--- app/models/contact_inquiry.rb
+++ app/models/contact_inquiry.rb
@@ -1,2 +1,7 @@
 class ContactInquiry < ActiveRecord::Base
+  validates :email, presence: true
+  validates :subject, presence: true
+  validates :description, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
--- spec/models/contact_inquiry_spec.rb
+++ spec/models/contact_inquiry_spec.rb
@@ -1,5 +1,42 @@
 require 'spec_helper'
 
 describe ContactInquiry do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:valid_attrs) { {
+    email: 'email@email.com',
+    subject: 'subject help',
+    description: 'How do I subject?',
+    first_name: 'Guy',
+    last_name: 'Incognito'
+    } }
+
+  it "has a valid email address" do
+    inquiry = ContactInquiry.new(valid_attrs.merge(email: ''))
+    inquiry.valid?
+    expect(inquiry.errors[:email]).to include "can't be blank"
+  end
+
+  it "has a subject" do
+    inquiry = ContactInquiry.new(valid_attrs.merge(subject: ''))
+    inquiry.valid?
+    expect(inquiry.errors[:subject]).to include "can't be blank"
+  end
+
+  it "has a description" do
+    inquiry = ContactInquiry.new(valid_attrs.merge(description: ''))
+    inquiry.valid?
+    expect(inquiry.errors[:description]).to include "can't be blank"
+  end
+
+  it "has a first name" do
+    inquiry = ContactInquiry.new(valid_attrs.merge(first_name: ''))
+    inquiry.valid?
+    expect(inquiry.errors[:first_name]).to include "can't be blank"
+  end
+
+  it "has a last name" do
+    inquiry = ContactInquiry.new(valid_attrs.merge(last_name: ''))
+    inquiry.valid?
+    expect(inquiry.errors[:last_name]).to include "can't be blank"
+  end
 end
